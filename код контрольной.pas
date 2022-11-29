4be8a4e899d4c1864630a8f5d7c240d4284c316c
YKDubrovina <117807857+YKDubrovina@users.noreply.github.com>
2022-11-29 16:23:58 +0300

    Add files via upload

---
+++ код контрольной.pas
@@ -0,0 +1,15 @@
+﻿program kontrolnaya;
+var x:real;
+begin
+ x:=-12;
+  while x<=3 do
+    begin
+if x<-10 then
+writeln('x = ',x:2:2, ' f = ',log(x)*log(x));
+if (-10<=x) and (x<1) then
+writeln('x = ',x:2:2, ' f = ',x**3*sin(x)+x/x**(1/3));
+if 1 <= x then
+writeln('x = ',x:2:2, ' f = ',(-x/x**(1/3))*(-x/sin(x)));
+x:=x+0.2;
+ end;
+end.
